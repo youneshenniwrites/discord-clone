a6b6e4fa190bdb31903cc2cba5cfe7c627a471d7
youneshenniwrites <youneshenniwrites@gmail.com>
2023-08-26 17:12:25 +0100

    setup initial profile using prisma

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+
+export default async function SetupPage() {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <div>Create a Server</div>;
+}
