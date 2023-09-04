34583b973fc1422f6e3a0226f812d47aa1c58034
youneshenniwrites <youneshenniwrites@gmail.com>
2023-09-04 11:42:00 +0100

    configure a web socket

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -28,8 +29,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
